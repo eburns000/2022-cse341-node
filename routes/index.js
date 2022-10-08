54c845e79549aa66674ca973462d569e80c7a036
Eric Burns <eburns000@byui.edu>
2022-10-08 08:02:52 -0700

    add swagger api-docs to project

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const swagger = require('./swagger');
 
+router.use('/', swagger);
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
